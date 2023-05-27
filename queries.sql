06b1f19d3ffb5e11551a3fe9ba31ee3e753c6e5f
mhdmd01 <119261970+mhdmd01@users.noreply.github.com>
2023-05-27 11:24:44 +0200

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,11 @@
+--1)somma delle views di un canale/video
+
+--2)query parametrica: barra di ricerca video/canale/topic
+
+--3)i tuoi video/preferiti/like/cronologia
+
+--4)video in tendenza(con limit 10)
+
+--5)video ordinati in base al rating ricevuto(like+dislike/views)
+
+--6)utenti a cui far vedere una ads(se hanno yt premium o sono abbonati ad un canale)
