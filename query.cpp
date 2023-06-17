a680043b9318dfab65d9f08d1800773785acb750
Davide Picello <119404292+DavidePicc@users.noreply.github.com>
2023-06-17 12:53:16 +0200

    Update query.cpp DEFINITIVO

--- query.cpp
+++ query.cpp
@@ -65,11 +65,39 @@ void stampaRes(PGresult* res){
         }
     }else if(tuple == 1){
         for (int i = 0; i < campi; ++i)
-            cout << capitalizeString(PQfname(res, i)) << ": " << PQgetvalue(res, 0, i) << endl;
+            cout << capitalizeString(PQfname(res, i)) << ": \t\t" << PQgetvalue(res, 0, i) << endl;
     }else{
         cout << "Nessun risultato " << endl;
     }
 }
+
+
+
+//Funzione di esecuzione della query con 0 parametri
+void queryProcedureNoParamNoOut(PGconn* conn, string query){
+    const char* par1 = query.c_str();
+
+    PGresult* res = PQexec (conn, par1) ;
+    cout << "\nQuery completata con successo !\n";
+
+    PQclear (res);
+}
+
+
+
+//Funzione di esecuzione della query con 0 parametri
+void queryProcedureNoParam(PGconn* conn, string query){
+    const char* par1 = query.c_str();
+
+    cout << "\nRisultati della ricerca: \n";
+
+    PGresult* res = PQexec (conn, par1) ;
+    checkResults(res, conn);
+
+    stampaRes(res);
+    PQclear (res);
+}
+
 
 
 //Funzione di esecuzione della query con 1 parametro (chiesto qui dentro)
@@ -88,6 +116,23 @@ void queryProcedure(PGconn* conn, string query){
     stampaRes(res);
     PQclear (res);
 }
+
+
+
+//Funzione di esecuzione della query con 1 parametro (passato dal main)
+void queryProcedure(PGconn* conn, string query, string p1){
+    PGresult* stmt = PQprepare(conn, "query_ricerca", query.c_str(), 1, NULL);
+    
+    const char* parameter = p1.c_str();
+    cout << "\nRisultati della ricerca: \n";
+
+    PGresult* res = PQexecPrepared(conn, "query_ricerca", 1, &parameter, NULL, 0, 0);
+    checkResults(res, conn);
+
+    stampaRes(res);
+    PQclear (res);
+}
+
 
 
 //Funzione di esecuzione della query con 2 parametri (passati dal main)
@@ -120,12 +165,29 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    //Menu per ricerca video/canale/topic
+    //Menu per ricerca
     int scelta = -1;
 
     do{
-        cout << "Selezionare l'opzione desiderata: \n 0) Terminare ricerca\n 1) Ricerca video\n 2) Ricerca canale\n 3) Ricerca video per categoria\n 4) Ricerca canale per categoria\n 5) Ricerca video di un canale\n 6) Ricerca video di una categoria specifica appartenenti allo stesso canale \n\n"
-            << "Inserire scelta: ";
+        cout << "Selezionare l'opzione desiderata: \n\n" <<
+                "Queries parametriche\n" <<
+                "0) Terminare ricerca\n" <<
+                "1) Ricerca video\n" <<
+                "2) Ricerca canale\n" <<
+                "3) Ricerca video per categoria\n" <<
+                "4) Ricerca canale per categoria\n" <<
+                "5) Ricerca video di un canale\n" <<
+                "6) Ricerca video di una categoria specifica appartenenti allo stesso canale \n\n" <<
+                "Queries SQL\n" <<
+                "7) Vedere le views totali di ogni canale\n" <<
+                "8) Creazione playlist ''Mi piace'' e ''Guarda piu tardi''\n" <<
+                "9) Aggiunta dei video piaciuti alla playlist ''Mi piace'' per ogni utente\n" <<
+                "10) Classifica video (video ordinati in base al rating ricevuto)\n" <<
+                "11) Video in tendenza\n" <<
+                "12) Utenti che vedranno la pubblicita\n" <<
+                "13) Live in corso\n" <<
+                "14) Visualizzazione commenti\n" <<
+                "\nInserire scelta: ";
         cin >> scelta;
         cin.ignore();
 
@@ -176,6 +238,58 @@ int main(int argc, char **argv) {
                     string query = "SELECT A.handle, A.imgprofilo, V.thumbnail, V.titolo, V.descrizione, V.datapubblicazione, V.durata, V.costo FROM Video AS V JOIN account AS A ON V.id_account = A.id_account WHERE A.handle = $1::varchar(256) AND V.categoria = $2::Categorie AND visibilita = 'Pubblico'";
                     queryProcedure(conn, query, in1, in2);
                 }
+            break;
+
+            case 7:{
+                    string query = "DROP VIEW IF EXISTS ViewsPerVideo; CREATE VIEW ViewsPerVideo AS(SELECT id_Video, COUNT(account) AS total_views FROM Views GROUP BY id_Video); SELECT Video.id_account, SUM(total_views) AS ViewsCanale FROM ViewsPerVideo, Video GROUP BY Video.id_account;";
+                    queryProcedureNoParam(conn, query);
+            }
+            break;
+
+            case 8:{
+                    string query = "INSERT INTO Playlist (account, titolo, descrizione, visibilita) SELECT id_Account, 'Guarda più tardi', 'I tuoi video da guardare più tardi', 'Privato' FROM account; INSERT INTO Playlist (account, titolo, descrizione, visibilita) SELECT id_Account, 'Video piaciuti', 'I video a cui hai messo like', 'Privato' FROM account; INSERT INTO VideoPlaylist(id_Video,id_Playlist) SELECT L.id_Video, P.id_Playlist FROM Playlist AS P, LikeVideo AS L WHERE L.valutation='1' AND L.account=P.account AND P.titolo='Video piaciuti';";
+                    queryProcedureNoParamNoOut(conn, query);
+            }
+            break;
+
+            case 9:{
+                    string query = "INSERT INTO VideoPlaylist(id_Video,id_Playlist) SELECT L.id_Video, P.id_Playlist FROM Playlist AS P, LikeVideo AS L WHERE L.valutation='1' AND L.account=P.account AND P.titolo='Video piaciuti'";
+                    queryProcedureNoParamNoOut(conn, query);
+            }
+            break;
+
+            case 10:{
+                    string query = "DROP VIEW IF EXISTS ViewsPerVideo; DROP VIEW IF EXISTS SommaLike; DROP VIEW IF EXISTS voto; CREATE VIEW ViewsPerVideo AS(SELECT id_Video, COUNT(account) AS total_views FROM Views GROUP BY id_Video); CREATE VIEW voto AS(SELECT id_Video, CASE WHEN valutation = '1' THEN 1.0 ELSE -1.0 END AS stato FROM LikeVideo); CREATE VIEW SommaLike AS(SELECT id_Video, SUM(stato) AS SommaLike FROM voto GROUP BY id_Video); SELECT S.id_Video, (S.SommaLike/V.total_views) AS RATING FROM SommaLike AS S JOIN ViewsPerVideo AS V ON S.id_Video=V.id_Video ORDER BY RATING DESC;";
+                    queryProcedureNoParam(conn, query);
+            }
+            break;
+
+            case 11:{
+                    string query = "SELECT V.* FROM Video AS V JOIN (SELECT id_Video, COUNT(*) AS num_views FROM Views GROUP BY id_Video) AS VIEWS ON V.id_Video = VIEWS.id_Video ORDER BY VIEWS.num_views DESC LIMIT 10;";
+                    queryProcedureNoParam(conn, query);
+            }
+            break;
+
+            case 12:{
+                    string query = "SELECT * FROM account WHERE premium = FALSE;";
+                    queryProcedureNoParam(conn, query);
+            }
+            break;
+
+            case 13:{
+                    string query = "SELECT * FROM Video WHERE isLive = true;";
+                    queryProcedureNoParam(conn, query);
+            }
+            break;
+
+            case 14:{
+                    string query, in1;
+                    cout << "Inserire l'ID del video (per esempio 4): ";
+                    getline(cin, in1); 
+                    query = "SELECT  * FROM Commenti WHERE id_video = $1::integer ORDER BY datacommento";
+                    queryProcedure(conn, query, in1);
+
+            }
             break;
         }
         cout << "\n-----------------------------------\n\n";
